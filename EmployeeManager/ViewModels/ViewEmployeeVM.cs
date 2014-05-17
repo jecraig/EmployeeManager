6d4e0cdbfd7fc537488a1b2a0cc19a0bf3f5c191
Jacob E. Craig <beakerbite@gmail.com>
2014-05-17 13:29:45 -0600

    Setup some basic work on the view model for the employee view.

--- EmployeeManager/ViewModels/ViewEmployeeVM.cs
+++ EmployeeManager/ViewModels/ViewEmployeeVM.cs
@@ -1,12 +1,90 @@
-﻿using System;
+﻿using EmployeeManager.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace EmployeeManager.ViewModels
 {
 	class ViewEmployeeVM
 	{
+		private Employee modelEmployee;
+		public Employee ModelEmployee
+		{
+			get { return modelEmployee; }
+			set
+			{
+				modelEmployee = value;
+				OnPropertyChanged("ModelEmployee");
+			}
+		}
+
+		private ICommand saveEmployeeCommand;
+		public ICommand SaveEmployeeCommand
+		{
+			get { return saveEmployeeCommand; }
+			set
+			{
+				saveEmployeeCommand = value;
+				OnPropertyChanged("SaveEmployeeCommand");
+			}
+		}
+
+		public ViewEmployeeVM()
+		{
+			InitializeCommand();
+			LoadEmployee();
+		}
+
+		private void InitializeCommand()
+		{
+			SaveEmployeeCommand = new SaveEmployeeCommand(UpdatePerson);
+		}
+
+		private void UpdatePerson()
+		{
+			ModelEmployee.FirstName += "Ha";
+		}
+
+		private void LoadEmployee()
+		{
+			ModelEmployee = new Employee()
+			{
+				FirstName = "Brian",
+				LastName = "Lagunas",
+			};
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanged(string property)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(property));
+		}
+	}
+
+	public class SaveEmployeeCommand : ICommand
+	{
+		Action executeMethod;
+
+		public SaveEmployeeCommand(Action updateEmployee)
+		{
+			executeMethod = updateEmployee;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+
+		public event EventHandler CanExecuteChanged;
+
+		public void Execute(object parameter)
+		{
+			executeMethod.Invoke();
+		}
 	}
 }
