647794a3dfbec53539db5e2ff56262e555798a8f
Jacob E. Craig <beakerbite@gmail.com>
2014-05-17 13:50:18 -0600

    Got some basic layout complete for the employee view

--- EmployeeManager/Views/ViewEmployee.xaml.cs
+++ EmployeeManager/Views/ViewEmployee.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeManager.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace EmployeeManager.Views
 		public ViewEmployee()
 		{
 			InitializeComponent();
+			var viewModel = new	ViewEmployeeVM();
+			DataContext = viewModel;
 		}
 	}
 }
