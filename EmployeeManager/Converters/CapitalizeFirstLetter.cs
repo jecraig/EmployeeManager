20ffb0f7801072fb8a917e3dfabd3acd2315f7d6
Jacob E. Craig <beakerbite@gmail.com>
2014-05-17 23:52:22 -0600

    Fixed the data binding issue with the itemlist and added a converter so it displays properly.

---
+++ EmployeeManager/Converters/CapitalizeFirstLetter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EmployeeManager.Converters
+{
+
+	[ValueConversion(typeof(bool), typeof(bool))]
+	public class CapitalizeFirstLetter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is string)
+			{
+				var castValue = (string)value;
+				if (castValue.Count() == 0)
+					return castValue;
+				else
+					return char.ToUpper(castValue[0]) + castValue.Substring(1);
+			}
+			else
+			{
+				return value;
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value;
+		}
+	}
+}
